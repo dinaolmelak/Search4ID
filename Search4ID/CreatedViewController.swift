06e8d4e0042c764ec85a4066c7e0ce60c00f2518
Dinaol Tadesse <dinaolmelak@gmail.com>
2019-03-24 09:31:12 -0500

    first commit; Started the app

---
+++ Search4ID/CreatedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreatedViewController.swift
+//  Search4ID
+//
+//  Created by Dinaol Melak on 3/24/19.
+//  Copyright © 2019 Dinaol Melak. All rights reserved.
+//
+
+import UIKit
+
+class CreatedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Search4ID/ViewController.swift
+++ Search4ID/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldLabel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func didTapOnFound(_ sender: Any) {
+        
+        
+    }
+    
 
 }
 
